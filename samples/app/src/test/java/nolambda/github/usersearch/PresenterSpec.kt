5df7fbd06883aedf74d08fcdf0ec95cc9fb1d696
Esa Firman <esafirm21@gmail.com>
2021-01-03 14:20:45 +0700

    update presenter spec

--- samples/app/src/test/java/nolambda/github/usersearch/PresenterSpec.kt
+++ samples/app/src/test/java/nolambda/github/usersearch/PresenterSpec.kt
@@ -1,13 +1,12 @@
 package nolambda.github.usersearch
 
-import io.kotlintest.TestCase
-import io.kotlintest.TestResult
-import io.kotlintest.specs.AbstractStringSpec
-import io.kotlintest.specs.StringSpec
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.core.test.TestCase
+import io.kotest.core.test.TestResult
 import io.mockk.clearAllMocks
 import stream.nolambda.karma.Karma
 
-open class PresenterSpec(body: AbstractStringSpec.() -> Unit) : StringSpec({
+abstract class PresenterSpec(body: StringSpec.() -> Unit) : StringSpec({
     Karma.setTestMode()
     body.invoke(this)
 }) {
